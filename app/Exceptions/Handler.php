b9360a2620be94bad6b9cedc87a4c29b2d7b054e
Rockroc <mrxrpeng@gmail.com>
2017-06-14 10:05:34 +0800

    update great rule

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
@@ -44,6 +45,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+//        dd($exception);
+        if($exception instanceof RequestException){
+            return response()->json(['搜不到此商品']);
+        }
         return parent::render($request, $exception);
     }
 
